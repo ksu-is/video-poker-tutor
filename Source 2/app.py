5e82171192c9ff41db62031484e1f5c2caa8eae6
LinghJackson <32064434+LinghJackson@users.noreply.github.com>
2019-12-09 11:09:10 -0500

    12/9 Update
    Update to project so Nicole can look into analysis.py

--- Source 2/app.py
+++ Source 2/app.py
@@ -5,9 +5,12 @@ from card import Card
 from score import score_hand
 from analyze import analyze_hand
 
+from deck import Deck
+from hand import Hand
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/pick_hand')
 def pick_hand():
     rank_dict = {
         2:'2',
@@ -55,6 +58,54 @@ def analyze(hand_string):
     context = {'cards':cards, 'score':score, 'hold_cards':hold_cards, 'winning_hand':winning_hand, \
             'plays':plays_context, 'ev':ev_context}
     return render_template('analyze.html', **context)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+#game variables
+
+game_deck = Deck()
+game_hand = Hand()
+#edit later
+cards = []
+card_list = [Card(item) for item in cards]
+ev_list = 0
+plays_context = {}
+ev_context = {}
+
+def start_round():
+        global game_deck
+        global game_hand
+        global cards
+        global card_list
+        global ev_list
+        global plays_context
+        global ev_context
+        
+        game_deck = Deck()
+        game_hand = Hand()
+        for i in range(0,5):
+            game_hand.add_card(game_deck.draw_card())
+        print(game_hand)
+        cards = str(game_hand).split('  ')
+        cards.pop() #To prevent errors
+        card_list = [Card(item) for item in cards]
+        ev_list = analyze_hand(card_list)
+        plays_context = [get_play_string(row[0]) for row in ev_list]
+        ev_context = [row[1] for row in ev_list]
+        return False
+#end edit
+
+@app.route('/game')
+def game():
+    
+    start_round()
+    print("Export Cards\t", cards)
+    context = {'cards':cards, 'plays':plays_context, 'ev':ev_context}
+    return render_template('game.html', **context)
+#end edit
 
 def get_play_string(input_list):
     output_string = ''
